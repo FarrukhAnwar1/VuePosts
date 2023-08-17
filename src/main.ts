25bb85c42a02c06ea846fd7f519457bc122115dc
DMG1Plays <darksidefba@outlook.com>
2023-08-17 11:36:41 -0400

    updated firebase config

--- src/main.ts
+++ src/main.ts
@@ -5,14 +5,14 @@ import { createPinia } from "pinia";
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAUkk9hmeBrUeaYING3pE74EARmGCykHaU",
-  authDomain: "test-628e9.firebaseapp.com",
-  databaseURL: "https://test-628e9-default-rtdb.firebaseio.com",
-  projectId: "test-628e9",
-  storageBucket: "test-628e9.appspot.com",
-  messagingSenderId: "912708423531",
-  appId: "1:912708423531:web:6dddc325b5bf8e3041862e",
-  measurementId: "G-6E8GTLD04S"
+    apiKey: "AIzaSyAUkk9hmeBrUeaYING3pE74EARmGCykHaU",
+    authDomain: "test-628e9.firebaseapp.com",
+    databaseURL: "https://test-628e9-default-rtdb.firebaseio.com",
+    projectId: "test-628e9",
+    storageBucket: "test-628e9.appspot.com",
+    messagingSenderId: "912708423531",
+    appId: "1:912708423531:web:6dddc325b5bf8e3041862e",
+    measurementId: "G-6E8GTLD04S"
 };
 initializeApp(firebaseConfig);
 
